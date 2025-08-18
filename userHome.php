61b9b71a2abb54ad3bf0d8227d4a5aff603f1944
sabrin-liza <ssliza3028@gmail.com>
2025-08-19 03:24:20 +0600

    edits

--- userHome.php
+++ userHome.php
@@ -53,7 +53,7 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bachelor's Point - User Home</title>
+    <title>User Home</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         .wave-bg {
